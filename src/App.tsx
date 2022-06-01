50174eaeacd13fd562ff45252e25282bc3049090
abner luan <abnerluan7@gmail.com>
2022-05-31 23:56:01 -0300

    update to pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 
 import AuthenticatedLayout from '@/layouts/AuthenticatedLayout/AuthenticatedLayout'
 
-import AddContact from '@/components/AddContact'
-import Contact from '@/components/Contact'
-import ContactsList from '@/components/ContactsList'
+import AddContact from '@/pages/AddContact'
+import Contact from '@/pages/Contact'
+import ContactsList from '@/pages/ContactsList'
 
 const App: React.FC = () => {
   return (
